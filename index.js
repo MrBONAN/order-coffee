ee11850e50c50ff4857d698f4c41cff06a7cbf8d
kusSsa <kuskovaevgenia3083@gmail.com>
2025-04-22 17:44:13 +0500

    первое задание update

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 document.querySelector('.add-button').addEventListener('click', () => {
     const form = document.querySelector('form');
+    const firstBeverage = form.querySelector('.beverage');
     const beverages = form.querySelectorAll('.beverage');
-    const lastBeverage = beverages[beverages.length - 1];
-    const clone = lastBeverage.cloneNode(true);
+    const clone = firstBeverage.cloneNode(true);
     const newNumber = beverages.length + 1;
     clone.querySelector('.beverage-count').textContent = `Напиток №${newNumber}`;
-    clone.querySelector('select').selectedIndex = 0;
+    clone.querySelector('select').selectedIndex = 1;
+    const newRadioName = 'milk-' + Date.now();
     const radios = clone.querySelectorAll('input[type="radio"]');
-    radios.forEach((radio, i) => radio.checked = i === 0);
+    radios.forEach((radio, i) => {
+        radio.name = newRadioName;
+        radio.checked = i === 0;
+    });
     clone.querySelectorAll('input[type="checkbox"]').forEach(chk => chk.checked = false);
-    form.insertBefore(clone, form.querySelector('.add-button').parentElement);
+    form.insertBefore(clone, document.querySelector('.add-button').parentElement);
 });
-
